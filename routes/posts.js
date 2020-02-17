9c1ec210de6a115dbfc531d30d429d9e6c76b767
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-02-17 10:58:10 -0600

    modified posts.js file

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* Get All posts  */
 
 router.get('/', function(req, res, next) {
-  res.render('posts');
+  res.render('posts',{
+    title: 'Post One',
+    author: 'Mahmoud Osman',
+    body: 'This is the main post body'
+  });
 });
 module.exports = router;
