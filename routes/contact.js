d303cc239f9260d774169525319d03f29e25069f
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-02-27 20:58:36 -0600

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET contact page. */
 
-router.get('/contact', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('contact');
 });
 module.exports = router;
