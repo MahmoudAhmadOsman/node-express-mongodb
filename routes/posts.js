12dd15afba86e746d126fee15844972de8b5ecb3
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-02-27 20:59:09 -0600

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Get All posts  */
 
-router.get('/posts', function(req, res, next) {
+router.get('/', function(req, res, next) {
  const Post =  Post.find({}) // Query posts from the database 
   res.render('posts',{
     title: 'Post One',
