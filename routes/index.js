f74ff5d69117173fecb6ec498f0e49277e3f2752
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-03-01 19:18:55 -0600

    added product page and route

--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Welcome to my page!' });
-// });
-
-
-router.get('/', function(req, res, next){
-  Post.find({}, function(err, posts){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('index', {
-        title:'posts',
-        posts: posts
-      });
-    }
-  });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome to my page!' });
 });
 
 
-// router.get('/', function(req, res) {
-//   console.log('All posts');
-//   Post.find({})
-//     .exec(function(err, posts) {
-//       if(err) {
-//         res.send('error occured')
-//       } else {
-//         console.log(posts);
-//         res.json(posts);
-//       }
-//     });
-// });
+
+
+
 
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* Get All posts  */
 
 router.get('/', function(req, res, next) {
- const Post =  Post.find({}) // Query posts from the database 
+ //const Post =  Post.find({}) // Query posts from the database 
   res.render('posts',{
     title: 'Post One',
     author: 'Mahmoud Osman',
